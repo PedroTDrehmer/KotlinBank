c29878539d6ac367e7c0930de9b4505c021d6787
PedroTDrehmer <pedrodrehmer@outlook.com>
2022-09-02 23:36:45 -0300

    Created class Owner

---
+++ src/main/kotlin/accountOwners/Owner.kt
@@ -0,0 +1,7 @@
+package accountOwners
+
+class Owner {
+    val holder = "Nina"
+    val accountNumber = 1000
+    var balance = 0.0
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,16 @@
+import accountOwners.Owner
+
 fun main() {
 
-    val holder: String = "Account Owner"
-    val accountNumber: Int = 1000
-    var balance: Double = 0.0
+    val nina = Owner()
 
-    println("Holder $holder")
-    println("Account Number $accountNumber")
-    println("Account Balance $balance")
+    println("Holder ${nina.holder}")
+    println("Account Number ${nina.accountNumber}")
+    println("Account Balance ${nina.balance}")
 
-    when {
-        balance > 0.0 -> println("Positive")
-        balance == 0.0 -> println("Neutral")
-        else -> println("Negative")
-    }
+//    when {
+//        balance > 0.0 -> println("Positive")
+//        balance == 0.0 -> println("Neutral")
+//        else -> println("Negative")
+//    }
 }
