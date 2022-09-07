41a576891569d426fbfe847fba915c5315e1da01
PedroTDrehmer <pedrodrehmer@outlook.com>
2022-09-07 12:19:07 -0300

    Create deposit and withdraw

--- src/main/kotlin/accounts/Account.kt
+++ src/main/kotlin/accounts/Account.kt
@@ -5,4 +5,12 @@ class Account(
     val accountNumber: Int
 ) {
     var balance: Double = 0.0
+
+    fun deposit(value: Double) {
+        this.balance += value
+    }
+
+    fun withdraw(value: Double) {
+        this.balance -= value
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,14 +6,11 @@ fun main() {
         holder = "Nina",
         accountNumber = 1000
     )
+    account1.deposit(50.0)
+    account1.withdraw(40.0)
 
     println("Holder ${account1.holder}")
     println("Account Number ${account1.accountNumber}")
     println("Account Balance ${account1.balance}")
 
-//    when {
-//        balance > 0.0 -> println("Positive")
-//        balance == 0.0 -> println("Neutral")
-//        else -> println("Negative")
-//    }
 }
